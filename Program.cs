9b34f32745e922854d92d4b48ffe77fed03c0716
DESKTOP-G085NET\csi23-nrojlla <csi23-nrojlla@colegioaltair.net>
2024-02-05 14:38:58 +0100

    RestaFecha

--- Program.cs
+++ Program.cs
@@ -5,19 +5,26 @@
         static void Main(string[] args)
         {
             DateTime dateTime = DateTime.Now;//instante de hoy
-            int suma = 1 + dateTime.Day;
-            int meses = dateTime.Month - 24;
+            DateTime resultado = dateTime.AddDays(1).AddMonths(-24);//esta restando 2 años al año
+            
             Random random = new Random();
             int[] numeros = { 0, 1, 2, 3, 4 };
-
             int numeroR = random.Next(numeros.Length);
-            int randomYear = dateTime.Year + numeroR;
 
-            DateTime resultado = dateTime.AddDays(suma);
-            resultado = dateTime.AddMonths(meses);
-            resultado = dateTime.AddYears(numeroR);
-
+            resultado = resultado.AddYears(numeroR); 
+            
+            Console.WriteLine("Resultado Inicial: " + dateTime );
             Console.WriteLine("Resultado final: " + resultado );
+
+            if (dateTime < resultado)
+            {
+                Console.WriteLine("El hoy es más grande");
+            }
+            else
+            {
+                Console.WriteLine("La fecha Inicial es menor que el resultado");
+            }
+            //fcha1,addfays(1).addmontons-(24)
 
         }
     }
