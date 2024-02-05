2249f0366e779e67ee5111854e982ef0f59fc691
DESKTOP-G085NET\csi23-nrojlla <csi23-nrojlla@colegioaltair.net>
2024-02-05 12:42:54 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MétodosDateTimeEjercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
